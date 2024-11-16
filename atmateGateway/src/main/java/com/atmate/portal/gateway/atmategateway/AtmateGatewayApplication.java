f5be2917bdfe7159edfcb7983ce2cb286dcd82b0
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2024-11-16 17:34:36 +0000

    Esqueleto e estrutura do projeto
    Commit inicial com a base e o esqueleto do projeto

---
+++ atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/AtmateGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.atmate.portal.gateway.atmategateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmateGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmateGatewayApplication.class, args);
+    }
+
+}
---
+++ atmateGateway/src/test/java/com/atmate/portal/gateway/atmategateway/AtmateGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmate.portal.gateway.atmategateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmateGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
