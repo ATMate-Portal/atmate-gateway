ce7c3bc5b53c120f7cf1d75b50baaf5c461144bd
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-03-15 20:57:28 +0000

    Corrigida erro de CORS

--- atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/controller/TaxController.java
+++ atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/controller/TaxController.java
@@ -3,14 +3,12 @@ package com.atmate.portal.gateway.atmategateway.controller;
 import com.atmate.portal.gateway.atmategateway.database.dto.UrgentTaxResponseDTO;
 import com.atmate.portal.gateway.atmategateway.database.services.TaxService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "${cors.allowed.origin}") // Corrected line
 @RequestMapping("/tax")
 public class TaxController {
 
