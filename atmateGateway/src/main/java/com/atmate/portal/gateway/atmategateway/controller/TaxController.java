70f6ff467d54664191d79b2a6587f193ca69154c
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-04-26 15:53:07 +0100

    Adicionado controlo de chamadas às taxas urgentes

--- atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/controller/TaxController.java
+++ atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/controller/TaxController.java
@@ -1,18 +1,28 @@
 package com.atmate.portal.gateway.atmategateway.controller;
 
 import com.atmate.portal.gateway.atmategateway.database.dto.OperationHistoryRequestDTO;
+import com.atmate.portal.gateway.atmategateway.database.dto.ParamsDTO;
 import com.atmate.portal.gateway.atmategateway.database.dto.TaxResponseDTO;
 import com.atmate.portal.gateway.atmategateway.database.dto.UrgentTaxResponseDTO;
+import com.atmate.portal.gateway.atmategateway.database.entitites.Configuration;
+import com.atmate.portal.gateway.atmategateway.database.services.ConfigurationService;
 import com.atmate.portal.gateway.atmategateway.database.services.OperationHistoryService;
 import com.atmate.portal.gateway.atmategateway.database.services.TaxService;
+import com.atmate.portal.gateway.atmategateway.utils.enums.ErrorEnum;
+import com.atmate.portal.gateway.atmategateway.utils.exceptions.ATMateException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*" /*${cors.allowed.origin}"*/) // Corrected line
 @RequestMapping("/tax")
+@Slf4j
 public class TaxController {
 
     @Autowired
@@ -20,11 +30,23 @@ public class TaxController {
 
     @Autowired
     OperationHistoryService operationHistoryService;
+    @Autowired
+    ConfigurationService configurationService;
 
+    @Value("${warning.days.value:warning_days}")
+    private String warningDaysVarName;
     @GetMapping("/getUrgentTaxes")
     public List<UrgentTaxResponseDTO> getUrgentTaxes(@RequestParam Integer days) {
 
         List<UrgentTaxResponseDTO> urgentTaxResponseDTOList = taxService.getUrgentTaxes(days);
+
+        Optional<Configuration> configuration = configurationService.getConfigurationString(warningDaysVarName);
+
+        String param = configuration.map(Configuration::getVarvalue).orElse(null);
+
+        if(!Objects.equals(param, String.valueOf(days))){
+            throw new ATMateException(ErrorEnum.GENERIC_ERROR);
+        }
 
         OperationHistoryRequestDTO operationHistoryRequestDTO = new OperationHistoryRequestDTO();
         operationHistoryRequestDTO.setActionCode("CHECK-001");
