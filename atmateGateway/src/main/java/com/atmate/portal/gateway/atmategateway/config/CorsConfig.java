6fefce72b6094d13ac600bacf5556e8b03bafbda
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-05-01 11:19:52 +0100

    Update CorsConfig.java

--- atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/config/CorsConfig.java
+++ atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/config/CorsConfig.java
@@ -27,11 +27,7 @@ public class CorsConfig {
         // 🌍 !!! IMPORTANTE: DEFINIR A ORIGEM EXATA DO FRONTEND !!!
         // Substitua "http://localhost:5173" pela URL exata onde o seu React App corre.
         // Adicione outras URLs de produção se necessário. NÃO USE "*" com allowCredentials=true.
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173" // <--- VERIFIQUE E AJUSTE ESTA URL
-                , "http://atmate.sytes.net/"
-                , "http://atmate.sytes.net:4173/"// Exemplo produção
-        ));
+        config.setAllowedOrigins(List.of("*"));
         // config.setAllowedOriginPatterns(List.of("*")); // <-- REMOVER/COMENTAR
 
         // ✅ Métodos HTTP permitidos
