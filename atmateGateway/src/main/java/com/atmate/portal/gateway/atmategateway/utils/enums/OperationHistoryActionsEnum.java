e4c9624e366da5db960478903c9adbe8946c97e2
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2025-05-27 21:36:57 +0100

    Update OperationHistoryActionsEnum.java

--- atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/utils/enums/OperationHistoryActionsEnum.java
+++ atmateGateway/src/main/java/com/atmate/portal/gateway/atmategateway/utils/enums/OperationHistoryActionsEnum.java
@@ -66,7 +66,7 @@ public enum OperationHistoryActionsEnum {
 
     UPDATE_CONFIG_NOTIFICATION("UPD-002", "Utilizador {0} atualizou a notificação {1}."),
 
-    FORCE_SEND_NOTIFICATION("FORCE-SEND-001", "Foram enviadas {0} notificações"),
+    FORCE_SEND_NOTIFICATION("FORCE-SEND-001", "Utilizador {0} enviou {1} notificações"),
 
     /**
      * Modification of configuration deadlines.
